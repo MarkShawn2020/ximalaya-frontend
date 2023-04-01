017bf589f9639eb536fc0b15e36d60e1f9d60bf6
markshawn2020 <shawninjuly@gmail.com>
2023-04-01 11:05:01 +0800

    moveed data

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import album1 from '../data/36817660.json'
-import album2 from '../data/37750423.json'
+import album1 from './data/36817660.json'
+import album2 from './data/37750423.json'
 
 export const ximalayaListData = [...album1, ...album2]
